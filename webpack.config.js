7a7d3e88c8c25a31d7709131ba5b99c41bc5e14d
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-11-05 20:21:49 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,9 @@ module.exports = {
         ],
     },
     externals: {
-        // shows how we can rely on browser globals instead of bundling these dependencies,
-        // in case we want to access jQuery from a CDN or if we want an easy way to
-        // avoid loading all moment locales: https://github.com/moment/moment/issues/1435
-        jquery: '$',
-        moment: 'moment'
     },
     resolve: {
-        extensions: [ '*', '.ts', 'js', 'css' ]
+        extensions: [ '*', '.ts', '.js', '.css' ]
     },
     optimization: {
         minimizer: [new UglifyJsPlugin({
